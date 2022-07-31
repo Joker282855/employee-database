6a78ccc496a2da9d91bae9724c326a2717f3afc5
unknown <jtjackson325@gmail.com>
2022-07-31 16:57:28 -0600

    added the values to the seeds file and having to make sure the values are typed in correctly

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO empolyees (first_name, last_name, positions_id, assistant_manager_id)
+VALUES ('Susan', 'Anthony', 1),
+('Max', 'Mayfield', 2),
+('Happy', 'Dean', 3, 1),
+('Robert', 'Stallin', 4),
+('Cynthia', 'Drake', 5),
+('Berry', 'School', 6, 2),
+('Showy', 'Wednesday', 7),
+('Suzzy', 'Lipskins', 8),
+('Greg', 'Berkowhitz', 9, 3); 
+
