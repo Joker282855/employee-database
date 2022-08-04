ede39dcfe54642450566fc44b9d70def0f69040f
unknown <jtjackson325@gmail.com>
2022-08-03 20:06:25 -0600

    aded the positions table to the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,14 @@
-CREATE TABLE empolyees (
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS positions;
+
+CREATE TABLE positions (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8, 2) NOT NULL,
+    location_id INT NOT NULL
+);
+
+CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO empolyees (first_name, last_name, positions_id, assistant_manager_id)
+INSERT INTO employees (first_name, last_name, positions_id, assistant_manager_id)
 VALUES ('Susan', 'Anthony', 1, NULL),
 ('Max', 'Mayfield', 2, 1),
 ('Happy', 'Dean', 3, 1),
 ('Robert', 'Stallin', 4, 1),
 ('Cynthia', 'Drake', 5, 1),
-('Berry', 'School', 6, 1,),
+('Berry', 'School', 6, 1),
 ('Showy', 'Wednesday', 7, 1),
 ('Suzzy', 'Lipskins', 8, 1),
-('Greg', 'Berkowhitz', 9, 3); 
+('Greg', 'Berkowhitz', 9, 1); 
 
