580c66c4b90fc1d341836abbf1428bba2bf281f9
unknown <jtjackson325@gmail.com>
2022-08-09 14:43:13 -0600

    got the connection to the server up and working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,6 +8,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Coderrocks12!',
+        database: 'record'
+    },
+    console.log('Connected to the record database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
