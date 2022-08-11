904c0c2242dc7396c0c10621e57458f5bad65a10
unknown <jtjackson325@gmail.com>
2022-08-11 16:49:00 -0600

    got the routes for the get and post routes for the sql inquiries

--- routes/api/employees-routes.js
+++ routes/api/employees-routes.js
@@ -4,6 +4,37 @@ const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
 router.get('/cmployees', (req, res) => {
-    const sql = 'SELECT employees.*, positions.title AS positions_title FROM employees LEFT JOIN positions ON employees.positions_id = positions.id'; 
+    const sql = `SELECT employees.*, positions.title AS positions_title FROM employees LEFT JOIN positions ON employees.positions_id = positions.id`; 
 
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'employee information recieved',
+            data: rows
+        });
+    });
+});
+
+router.post('/employees', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'positions_id', 'chief_id')
+    if(errors) {
+        res.status(400).json({ errors: errors });
+    }
+    const sql = `INSERT INTO employees (first_name, last_name, positions_id, chief_id)
+                VALUES (?, ?, ?, ?)`
+    const params = [body.first_name, body.last_name, body.positions_id, body.chief_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'posted successfully',
+            data: body
+        });
+    });
 });
