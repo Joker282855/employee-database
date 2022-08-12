8cf998addf63f6e1d1ac467ddab41c95dac51239
unknown <jtjackson325@gmail.com>
2022-08-11 19:53:29 -0600

    got the post route for the locations table coded

--- routes/api/locations-routes.js
+++ routes/api/locations-routes.js
@@ -17,3 +17,26 @@ router.get('/locations', (req, res) => {
         });
     });
 });
+
+router.post('/locations', ({ body }, res) => {
+    const errors = inputCheck(body, 'name');
+    if (err) {
+        res.status(400).json({ error: errors });
+    }
+    
+    const sql = `INSERT INTO locations (name)
+                VALUES (?)`;
+    
+    const params = [body.name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Information posted to the table correctly',
+            data: body
+        });
+    });
+});
