89d28c8481ac640e85b2fb5d2c20ade1bbbafada
unknown <jtjackson325@gmail.com>
2022-10-01 16:27:37 -0600

    added more to the function that views the employees information

--- server.js
+++ server.js
@@ -78,7 +78,11 @@ function viewPosition(){
 };
 
 function viewEmployees(){
-    db.query("select employees.id, employees.first_name, employees.last_name, position.title, chief.first_name as chief_first, chief.last_name as chief_last, role.salary, locations.name as location from employees left join postitions on positions_id = positions.id")
+    db.query("select employees.id, employees.first_name, employees.last_name, position.title, chief.first_name as chief_first, chief.last_name as chief_last, role.salary, locations.name as location from employees left join postitions on positions_id = positions.id", (err, data) => {
+        if (err) throw err
+        console.table(data)
+        organizer()
+    })
 };
 
 const PORT = process.env.PORT || 3001;
